04b28f307aefd935401d7bbf537b8b0e1aea66ef
TkachYulia <47217447+TkachYulia@users.noreply.github.com>
2020-04-29 21:15:29 +0600

    first commit

---
+++ app/src/main/java/com/example/moviedbappwithmap/utils/AppConstants.kt
@@ -0,0 +1,12 @@
+package com.example.moviedbappwithmap.utils
+
+object AppConstants {
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val POSTER_BASE_URL = "http://image.tmdb.org/t/p/w185"
+    const val BACKDROP_BASE_URL = "http://image.tmdb.org/t/p/w780"
+    const val API_KEY = "c04c23e9e1f4e91d3678f76d840f986f"
+
+    const val MOVIE_ID = "movie_id"
+    const val ACCOUNT_ID = "account_id"
+    const val SESSION_ID = "session_id"
+}
